793937cbd22a1981df3afc7b2ff7035cdfbf7781
Davide Cristini <davcri91@gmail.com>
2015-01-25 00:12:16 +0100

    created installDependencies.py
    Still need to be completed

---
+++ installDependencies.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python	
+
+import subprocess
+
+# Check installed dependencies
+
+# Check root priviledges
+# TODO
+
+# Install PySide
+subprocess.call(["apt-get", "-y", "install", "python-pyside"])
