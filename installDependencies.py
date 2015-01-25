eb62ebd81c570fd954d3f2d9db2fda32e72314c8
Davide Cristini <davcri91@gmail.com>
2015-01-25 01:13:53 +0100

    updated installDependencies.py

--- installDependencies.py
+++ installDependencies.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python	
 
 import subprocess
+import os, sys
 
 # Check installed dependencies
-
-# Check root priviledges
 # TODO
 
-# Install PySide
-subprocess.call(["apt-get", "-y", "install", "python-pyside"])
+# Checking root priviledges
+# get the effective user ID
+euid = os.geteuid()
+
+# if euid has root privileges
+if euid != 0:
+	print "You have to run this script with administrator priviledges"
+else:
+	# Install PySide
+	subprocess.call(["apt-get", "-y", "install", "python-pyside"])
